260c4a1e7d9a41a6a45eb555c658664a3e5f679f
Dastan Abdygali <dabdygal@lab1r2s14.42abudhabi.ae>
2025-05-05 11:56:41 +0400

    ex01 to ex11 done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let result = [];
+    for (const element of arr) {
+        if (!args.includes(element))
+            result.push(element);
+    }
+    return result;
 };
 
 // Do not edit below this line
