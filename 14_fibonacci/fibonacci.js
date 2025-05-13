95169dc088379bc04bbf9b6af233a241e7d9eb1d
Dastan Abdygali <dabdygali@gmail.com>
2025-05-13 11:35:57 +0400

    All done

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+	let num = Number(a);
+	if (Number.isNaN(num) || !Number.isFinite(num) || num < 0)
+		return "OOPS";
+	if (num === 0)
+		return 0;
+	a = 0;
+	b = 1;
+	for (let i = 1; i < num; i++) {
+		let tmp = b;
+		b = a + b;
+		a = tmp;
+	}
+	return b;
 };
 
 // Do not edit below this line
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+function getAge(obj) {
+	if ("yearOfDeath" in obj)
+		return obj.yearOfDeath - obj.yearOfBirth;
+	else
+		return new Date().getFullYear() - obj.yearOfBirth;
+}
+const findTheOldest = function(arr) {
+	return arr.reduce((accum, value) => {
+		if (getAge(value) > getAge(accum))
+			accum = value;
+		return accum;
+	});
 };
 
 // Do not edit below this line
